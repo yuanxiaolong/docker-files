c018495349b28e3f226e4641ac1954671e102421
yuanxiaolong <232351936@qq.com>
2014-11-05 18:04:28 +0800

    redis 代理

--- redis/proxy-slave/start-slave.sh
+++ redis/proxy-slave/start-slave.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
-#
-redis_master_porxy_ip=`cat /etc/hosts | grep redis_ambassador | awk '{print $1}'`
+#!/bin/sh
 
-if [ -z $redis_master_porxy_ip ]; then    
-    echo "redis_ambassador not defined in /etc/hosts Did you run with --link=redis_ambassador:redis_ambassador?";
-    exit 7;
+redis_master_porxy_ip=`cat /etc/hosts | grep redis | awk '{print $1}'`
+
+if [ -z $redis_master_porxy_ip ] 
+	then echo "redis_ambassador not defined in /etc/hosts Did you run with --link=redis_ambassador:redis_ambassador?"
 else
-    echo "redis_ambassador's ip -> " $redis_master_porxy_ip
-    exec /usr/local/bin/redis-server --slaveof $redis_master_porxy_ip 6379 
+	echo "redis_ambassador's ip -> " $redis_master_porxy_ip
+	exec /usr/local/bin/redis-server --slaveof $redis_master_porxy_ip 6379
 fi
-
-
 
