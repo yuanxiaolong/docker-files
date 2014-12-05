cd0415ec8f7b1e4adf84133520a54a06f0900975
yuanxiaolong <232351936@qq.com>
2014-12-05 17:24:11 +0800

    修改shell

--- redis/kube-slave/start-slave.sh
+++ redis/kube-slave/start-slave.sh
@@ -3,7 +3,7 @@
 # exec allows redis-server to receive signals for clean shutdown
 #
 
-echo "SERVICE_HOST -> " $SERVICE_HOST
+echo "REDISMASTER_SERVICE_HOST -> " $REDISMASTER_SERVICE_HOST
 echo "REDISMASTER_SERVICE_PORT -> " $REDISMASTER_SERVICE_PORT
 
-exec /usr/local/bin/redis-server --slaveof $SERVICE_HOST $REDISMASTER_SERVICE_PORT 
+exec /usr/local/bin/redis-server --slaveof $REDISMASTER_SERVICE_HOST $REDISMASTER_SERVICE_PORT 
