206bb22ced814dc2c3db9414bfbce0aad2779ca4
yuanxiaolong <232351936@qq.com>
2014-12-05 16:31:36 +0800

    change config

--- redis/kube-slave/start-slave.sh
+++ redis/kube-slave/start-slave.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 #
-if [ -z "$REDIS_MASTER_PORT_6379_TCP_ADDR" ]; then
-    echo "REDIS_MASTER_PORT_6379_TCP_ADDR not defined. Did you run with -link?";
-    exit 7;
-fi
 # exec allows redis-server to receive signals for clean shutdown
 #
 
